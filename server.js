9bfdee500152b7ced4b6a8193d02948829ed7b1e
Catarina <46010838+catmcc@users.noreply.github.com>
2020-07-24 13:11:37 +0100

    changed database host

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host  : '127.0.0.1',
+        host  : 'postgresql-dimensional-45541',
         user : 'postgres',
-        password : '1402',
+        password : '',
         database : 'smart-brain',
     }
 });
