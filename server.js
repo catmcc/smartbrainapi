25be2b456a06975c37cf0196274ee053aa4f3619
Catarina <46010838+catmcc@users.noreply.github.com>
2020-07-24 12:50:56 +0100

    added a comment

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`);
 })
 
+//routes
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { register.handleRegister(req,  res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
